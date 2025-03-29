2dbd87c8e07a16b28588ec84566aafdc10e8e102
KhumalosDS <security@khumalosdrivingschool.co.za>
2025-03-29 02:00:15 +0200

    Add files via upload

---
+++ icons.tsx
@@ -0,0 +1,19 @@
+
+import {
+  ArrowRight,
+  Twitter,
+  Apple,
+  Facebook,
+  Music2,
+  type LucideIcon,
+} from "lucide-react";
+
+export type Icon = LucideIcon;
+
+export const Icons = {
+  arrowRight: ArrowRight,
+  facebook: Facebook,
+  apple: Apple,
+  spotify: Music2,
+  twitter: Twitter,
+};
